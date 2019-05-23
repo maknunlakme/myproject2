f2ade00e62e4708485bc9ab2f90b3b50302781e6
maknunlakme <maknunsmail@gmail.com>
2019-05-23 19:57:29 +0600

    Add map

--- app/src/main/java/com/example/myproject2/MainActivity.kt
+++ app/src/main/java/com/example/myproject2/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.myproject2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val openMap : Button = findViewById(R.id.map_button)
+        openMap.setOnClickListener {
+            val intent = Intent(this, MapsActivity:: class.java)
+            startActivity(intent)
+        }
     }
 }
